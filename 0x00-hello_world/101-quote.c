30cdd8c16df706bc0d02c41eb57d5e7923f4463a
FSandimu <fransicahsandimu@gmail.com>
2023-09-07 06:51:33 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 /**
- * main is entry point
- * Return: 1 means (success)
- */
-int main() 
+*main - entry point
+*Return: 1 means (success)
+*/
+int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 60);
-    return 1;
+	const char *message = "and that piece of art is useful\"
+\- Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 60);
+	return (1);
 }
