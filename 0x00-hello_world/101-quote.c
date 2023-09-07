7ed836e83d2db44b1d37a1a5fe949bf7c2c243f9
FSandimu <fransicahsandimu@gmail.com>
2023-09-07 07:08:54 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
 *main - entry point
@@ -5,8 +6,6 @@
 */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
-
-	write(2, message, 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
