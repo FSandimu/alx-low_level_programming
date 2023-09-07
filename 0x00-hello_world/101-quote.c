0055fdabb768ebb3be93714a53322fc28a432260
FSandimu <fransicahsandimu@gmail.com>
2023-09-07 06:24:13 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -entry point
+ * Return: means 1 (success)
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
