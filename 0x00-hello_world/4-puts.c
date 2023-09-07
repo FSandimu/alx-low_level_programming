aff7006dd017e6dd9db4c695f5d86a3eea655e2f
FSandimu <fransicahsandimu@gmail.com>
2023-09-07 05:09:21 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+*main - is the entry point
+*puts - act like a printf but for strings only
+*return: returns 0 when called
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
