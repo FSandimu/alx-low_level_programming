7c27e46ea529e75ba0c691dc4d427690866b0f83
FSandimu <fransicahsandimu@gmail.com>
2023-09-08 05:43:51 -0700

    Spaces and commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: means 0(success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
