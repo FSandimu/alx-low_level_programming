ef78a34011bf937ae11d2721afaec64fd5639d7e
FSandimu <fransicahsandimu@gmail.com>
2023-09-08 04:55:53 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: means 0(success)
+ */
+int main(void)
+{
+	int p = 48;
+
+	while (p <= 57)
+	{
+		putchar(p);
+		p++;
+	}
+	putchar('\n');
+	return (0);
+}
