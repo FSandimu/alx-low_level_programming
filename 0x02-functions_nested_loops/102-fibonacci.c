46c411bce2f398b74576165a4b1b9dc48b6d3d55
FSandimu <fransicahsandimu@gmail.com>
2023-09-12 15:51:49 -0700

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: (success)
+ */
+int main(void)
+{
+	int a = 0;
+	long b = 1, k = 2;
+
+	while (a < 50)
+	{
+		if (a == 0)
+			printf("%ld", b);
+		else if (a == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += b;
+			b = k -b;
+			printf(", %ld", k);
+		}
+		++a;
+	}
+	printf("\n");
+	return (0);
+}
