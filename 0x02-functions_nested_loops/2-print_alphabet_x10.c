6dff7bdd45aa3159dfb04cb9be352a7a347131a6
FSandimu <fransicahsandimu@gmail.com>
2023-09-12 12:01:39 -0700

    Three

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,11 @@ void print_alphabet_x10(void)
 	int a;
 	int b;
 
-	for (b = 1; b <= 10; b++)
+	for (a = 1; b <= 10; b++)
 	{
 		for (a = 97; a <= 122; a++)
 		{
-			_putchar(b);
+			_putchar(a);
 		}
 		_putchar('\n');
 	}
