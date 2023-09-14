d50c77239327528381078d1fcf1729f3ea58107f
FSandimu <fransicahsandimu@gmail.com>
2023-09-14 15:36:24 -0700

    Nine

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - prints squares
+ * @size: number of squares/times
+ * Return: (success)
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
