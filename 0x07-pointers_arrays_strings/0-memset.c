43e44aad84015850e6a1b9687aeb31b9c10ec506
FSandimu <fransicahsandimu@gmail.com>
2023-09-25 07:48:05 -0700

    change

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
+		n--;
 	}
 	return (s);
 }
