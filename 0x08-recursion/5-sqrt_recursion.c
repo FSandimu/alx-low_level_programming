aa201fce40dc3cac457110e19421b78958e391f6
FSandimu <fransicahsandimu@gmail.com>
2023-09-27 13:05:22 -0700

    6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,9 +3,9 @@
 
 int _sqrt(int n, int i);
 /**
- * _sqrt - calculates natural square root
+ * _sqrt_recursion - calculates natural square root
  * @n: calculate the square root of a number
- * @i: iterate number
+ *
  * Return: success
  */
 int _sqrt_recursion(int n)
@@ -13,7 +13,7 @@ int _sqrt_recursion(int n)
 	return (_sqrt(n, 1));
 }
 /**
- * _sqrt_recursion - Return the natural square root of a number
+ * _sqrt - Return the natural square root of a number
  * @n: calculate the natural squre root
  * @i: mode
  * Return: natural squre root
