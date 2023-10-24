7f99f1a880ab5a3c4e84dc43eaa39e9b1100e09a
FSandimu <fransicahsandimu@gmail.com>
2023-10-24 14:38:42 -0700

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: list to be freed
+ * Return: nothing
+ */
+void free_listint(lisint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
